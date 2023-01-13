0b6139b2c7bef5ab738c538bdb8cdc64c01c06fd
Nikita-Fedorov <fedorov.nvad@gmail.com>
2023-01-13 22:10:03 +0200

    Add detection model into app

--- detector_app.py
+++ detector_app.py
@@ -1,32 +1,54 @@
+import io
+from PIL import Image
+
 import streamlit as st
-# import numpy as np
+import cv2
+import torch
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+import numpy as np
 # import pandas as pd
 # import time
 
+@st.cache(allow_output_mutation=True)
+def load_model():
+	# load a model pre-trained pre-trained on COCO
+	model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+	model = model.eval()
+	return model
+	
+def plot_predictions(numpy_img, preds, tresh=0.5):
+	boxes = preds['boxes'][preds['scores'] > tresh].detach().numpy()
+	for box in boxes:
+		numpy_img = cv2.rectangle(numpy_img.astype('float32'), (int(box[0]),int(box[1])), (int(box[2]),int(box[3])), (255, 0, 0), 3)
+	numpy_img = numpy_img.astype('uint')
+	st.image(numpy_img)
 
-
-# uploaded_file = st.sidebar.file_uploader("Upload a photo")
-# uploaded_file = st.sidebar.text_input("Or insert a link to the image")
+def detect_my_image(img_numpy, model, tresh):
+	img = torch.from_numpy(img_numpy.astype('float32')).permute(2,0,1)
+	img = img / 255.
+	predictions = model(img[None,...])
+	plot_predictions(img_numpy, predictions[0], tresh)
 
 def load_image():
 	uploaded_file = st.sidebar.file_uploader(label='Upload a photo')
-	# uploaded_file = st.text_input("Or insert a link to the image")
 	if uploaded_file is not None:
 		image_data = uploaded_file.getvalue()
 		st.image(image_data)
-		set_detect_params()
-
-def set_detect_params():
-	threshold = st.sidebar.slider("Set the threshold for the detector", 0., 1.)
-	clicked = st.sidebar.button('Detect objects')
-# with st.spinner('Please wait...'):
-# 	time.sleep(5)
-# 	# st.success('Done')
-# 	st.snow()
+		img_numpy = np.array(Image.open(io.BytesIO(image_data)))
+		return img_numpy
 
 def main():
 	st.title("Time to detect what you want")
-	load_image()
+	img = load_image()
+	if img is not None:
+		threshold = st.sidebar.slider("Set the threshold for the detector", 0., 1.)
+		model = load_model()
+		clicked = st.sidebar.button('Detect objects')
+		if clicked:
+			detect_my_image(img, model, threshold)
+			st.success('Done')
+			st.snow()
 	
 if __name__ == '__main__':
 	main()
