74d3f65630b84a232ef543df543c2705deaa6d0e
Nikita-Fedorov <fedorov.nvad@gmail.com>
2023-01-14 13:26:15 +0200

    Add labels for result image

--- detector_app.py
+++ detector_app.py
@@ -10,6 +10,21 @@ import numpy as np
 # import pandas as pd
 # import time
 
+coco_labels= ['person', 'bicycle', 'car', 'motorcycle', 'airplane',
+'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
+'street sign', 'stop sign', 'parking meter', 'bench', 'bird', 'cat',
+'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe',
+'hat', 'backpack', 'umbrella','shoe','eye glasses','handbag','tie',
+'suitcase','frisbee','skis','snowboard','sports ball','kite','baseball bat',
+'baseball glove','skateboard','surfboard','tennis racket','bottle',
+'plate','wine glass','cup','fork','knife','spoon','bowl','banana',
+'apple','sandwich','orange','broccoli','carrot','hot dog','pizza',
+'donut','cake','chair','couch','potted plant','bed','mirror','dining table',
+'window','desk','toilet','door','tv','laptop','mouse','remote','keyboard',
+'cell phone','microwave','oven','toaster','sink','refrigerator','blender',
+'book','clock','vase','scissors','teddy bear','hair drier','toothbrush',
+'hair brush']
+
 @st.cache(allow_output_mutation=True)
 def load_model():
 	# load a model pre-trained pre-trained on COCO
@@ -19,8 +34,21 @@ def load_model():
 	
 def plot_predictions(numpy_img, preds, tresh=0.5):
 	boxes = preds['boxes'][preds['scores'] > tresh].detach().numpy()
-	for box in boxes:
+	labels = preds['labels'][preds['scores'] > tresh].detach().numpy()
+	for i, box in enumerate(boxes):
 		numpy_img = cv2.rectangle(numpy_img.astype('float32'), (int(box[0]),int(box[1])), (int(box[2]),int(box[3])), (255, 0, 0), 3)
+		# coco_labels[labels[i]]
+		numpy_img = cv2.putText(numpy_img, 
+			coco_labels[labels[i]-1], 
+			(
+				int(box[0]),
+				int(box[1])-5
+			), 
+			cv2.FONT_HERSHEY_SIMPLEX,
+			fontScale = 2,
+			color = 255,
+			thickness = 3
+		)
 	numpy_img = numpy_img.astype('uint')
 	st.image(numpy_img)
 
